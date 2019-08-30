d0142dcd604bb36d357168ebc830318b09605251
zhenglei <zhenglei@5.cn>
2019-08-30 14:58:49 +0800

    glide 下载

--- app/src/main/java/com/gess/example/file/FileActivity.java
+++ app/src/main/java/com/gess/example/file/FileActivity.java
@@ -1,12 +1,19 @@
 package com.gess.example.file;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.blankj.utilcode.util.LogUtils;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.gess.example.R;
 
 public class FileActivity extends AppCompatActivity {
@@ -26,5 +33,16 @@ public class FileActivity extends AppCompatActivity {
                 file.copyFileToDir(MEDIA_FILE_TMP + "/filter.jpg", MEDIA_SAVE_DIR);
             }
         });
+
+        Glide.with(this)
+                .asBitmap()
+                .load("http://mimg.com.tv/img/2019/8/17/418226540F7EA75F13061F370A6DD17277C65738347E4FC797DCF52B8970F9D1")
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        ((ImageView) findViewById(R.id.iv_file)).setImageBitmap(resource);
+                    }
+                });
+
     }
 }
